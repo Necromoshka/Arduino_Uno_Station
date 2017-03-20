f013a445b571c7b58010ed0f58b85c104d0bb945
Necromoshka <sergeyklimets@gmail.com>
2017-03-20 20:55:20 +0300

    write prototype

--- sketch_for_Uno/pt2257_lib.cpp
+++ sketch_for_Uno/pt2257_lib.cpp
@@ -5,3 +5,30 @@
 /// \copyright Copyright (c) 2017 by Klimets Sergey.\n
 /// for Uno or nano
 #include "pt2257_lib.h"
+
+/*
+ * 
+ *     pt2257();
+    void set_volium(uint8_t);
+    void set_mute(boolean);
+  private:
+    uint8_t _vol;
+    boolean _mut;
+ */
+
+ pt2257::pt2257()
+ {
+   _vol = 0;
+   _mut = true;
+ }
+
+void pt2257::set_volium(uint8_t v)
+{
+  
+}
+
+void pt2257::set_mute(boolean m)
+{
+  
+}
+
--- sketch_for_Uno/pt2257_lib.h
+++ sketch_for_Uno/pt2257_lib.h
@@ -4,54 +4,64 @@
 /// \author Klimets Sergey
 /// \copyright Copyright (c) 2017 by Klimets Sergey.\n
 /// for Uno or nano
-#include "Arduino.h"
 # ifndef ENCODER_H
 # define ENCODER_H
+# define Addr 0x88
+# include "Arduino.h"
+# include "Wire.h"
+
 class pt2257
 {
   public:
     pt2257();
+    void init();
+    void set_volium(uint8_t);
+    void set_mute(boolean);
+  private:
+    uint8_t _vol;
+    boolean _mut;
+    void pt_write(uint8_t);
 };
-#endif
+# endif
 
 /*#include <Wire.h>
- 
-void setup() {
+
+  void setup() {
   Wire.begin();
-}
- 
-void loop() {
-Wire.beginTransmission(0x44);  // i2c adress pt2257
-Wire.write(0b11111111);        // задаем десятки
-Wire.write(0b11111111);        // задаем еденицы
-Wire.endTransmission();
-}
+  }
 
-Команды:
-для 2 каналов:
-0b01111000 //MUTE OFF
-0b01111001 //MUTE ON
-0b11111111 //Function OFF (-79dB)
+  void loop() {
+  Wire.beginTransmission(0x44);  // i2c adress pt2257
+  Wire.write(0b11111111);        // задаем десятки
+  Wire.write(0b11111111);        // задаем еденицы
+  Wire.endTransmission();
+  }
 
-0b11010000 // -0
-0b11010001 // -1
-0b11010010 // -2
-0b11010011 // -3
-0b11010100 // -4
-0b11010101 // -5
-0b11010110 // -6
-0b11010111 // -7
-0b11011000 // -8
-0b11011001 // -9
+  Команды:
+  для 2 каналов:
+  0b01111000 //MUTE OFF
+  0b01111001 //MUTE ON
+  0b11111111 //Function OFF (-79dB)
 
-0b11100000 // -00
-0b11100001 // -10
-0b11100010 // -20
-0b11100011 // -30
-0b11100100 // -40
-0b11100101 // -50
-0b11100110 // -60
-0b11100111 // -70
+  0b11010000 // -0
+  0b11010001 // -1
+  0b11010010 // -2
+  0b11010011 // -3
+  0b11010100 // -4
+  0b11010101 // -5
+  0b11010110 // -6
+  0b11010111 // -7
+  0b11011000 // -8
+  0b11011001 // -9
+
+  0b11100000 // -00
+  0b11100001 // -10
+  0b11100010 // -20
+  0b11100011 // -30
+  0b11100100 // -40
+  0b11100101 // -50
+  0b11100110 // -60
+  0b11100111 // -70
 
 */
 
