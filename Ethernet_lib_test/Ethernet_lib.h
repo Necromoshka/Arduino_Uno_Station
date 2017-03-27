7ba2774144d2cb36a73da3526ebbd19c9a483463
Necromoshka <sergeyklimets@gmail.com>
2017-03-27 07:12:28 +0300

    Test eyhercard lib

---
+++ Ethernet_lib_test/Ethernet_lib.h
@@ -0,0 +1,14 @@
+/// This work
+/// \file Ethernet_lib.h
+/// \brief main file
+/// \author Klimets Sergey
+/// \copyright Copyright (c) 2017 by Klimets Sergey.\n
+/// \base on http://playground.arduino.cc/Main/RotaryEncoders
+/// \base on https://www.pjrc.com/teensy/td_libs_Encoder.html#optimize
+/// for Uno or nano
+
+#include "Arduino.h"
+#ifndef ETHERNET_H
+#define ETHERNET_H
+
+#endif
---
+++ sketch_for_Uno/Ethernet_lib.cpp
@@ -0,0 +1,11 @@
+/// This work
+/// \file Ethernet_lib.cpp
+/// \brief main file
+/// \author Klimets Sergey
+/// \copyright Copyright (c) 2017 by Klimets Sergey.\n
+/// \base on http://playground.arduino.cc/Main/RotaryEncoders
+/// \base on https://www.pjrc.com/teensy/td_libs_Encoder.html#optimize
+/// for Uno or nano
+
+#include Ethernet_lib.h
+
