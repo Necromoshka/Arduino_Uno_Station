637c99c70c9f818d5534cf5eb92d0bf9200ad4a4
Necromoshka <sergeyklimets@gmail.com>
2017-04-12 19:09:18 +0300

    Address of pt2257 44 not 88

--- sketch_for_Uno/pt2257_lib.h
+++ sketch_for_Uno/pt2257_lib.h
@@ -9,7 +9,7 @@
 #include "Wire.h"
 #ifndef pt2257_H
 #define pt2257_H
-#define Addr 0x88
+#define Addr 0x44
 #define pt_off     0b11111111  // -79dB
 #define pt_1   0b11010000  // еденицы для 2 - х каналов
 #define pt_10  0b11100000  // десятки для 2 - х каналов
